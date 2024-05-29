68bdd1a888b1034fc1465fc92851328d4ecdb4e0
Narciso Castro <narcisocastro0802@gmail.com>
2024-05-29 16:42:02 -0700

    Declare and Initialize Variables

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,11 @@
-﻿namespace VariablesExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿//Declare and Initiallize variables
+
+string name = "Andy";
+int age = 28;
+char middleInitial = 'B';
+bool isOver18 = true;
+double currentHeight = 175.3;
+decimal currentPrices = 6.05m;
+
+Console.WriteLine($"Hello, My name is {name}, and I am {age} years old. My favorite inital in my name is {middleInitial} which would be my middle initial. Some people can tell I am over 18 years of age which is {isOver18}. I am 5 feet 9 and in centimeters that would be {currentHeight}cm. I live in California and gas prices here are around ${currentPrices}.");
+
